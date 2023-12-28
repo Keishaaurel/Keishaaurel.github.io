a6d9ba7d827dd6043a7a39826cf859e3b63bb8cd
Keishaaurel <keishaaureliarifai@gmail.com>
2023-12-28 15:30:34 +0700

    test

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 data = [{
-    'img1': 'assets/nagaga.jpg',
-    'img2': 'assets/loml.jpg',
-    'title': 'Web E-Commerce',
-    'subtitle': 'Zenit',
-    'desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam dolores quis expedita obcaecati! Blanditiis, labore!'
+    'img1': 'nagaga.jpg',
+    'img2': 'loml.jpg',
+    'title':'Web dan PUBG',
+    'subtitle':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, non.',
+    'desc':'Untuk project yang pertama adalah WEB saya pernah membuat sebuah web yang berisikan tentang biodata diri dan project yang kedua saya pernah memainkan geme PUBG bisa dibilang saya jago(diri sendiri) dan saya pernah menjadi top global '
+}, {
+    'img1': 'nagaga.jpg',
+    'img2': 'loml.jpg',
+    'title':'Capcut dan ChatGPT',
+    'subtitle':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, non.',
+    'desc':'Salah satu project ketiga saya adalah saya membuat vidio editing dan langsung dipamerkan di papan atas dan project keempat adalah saya pernah menyontek di ChatGPT'
 }]
 
 var container = document.querySelector('.container-project')
@@ -17,11 +23,8 @@ data.forEach(element => {
     </div>
     <div class="desc-project">
         <h1>${element.title}</h1>
-        <h2>${element.subtitle}</h2>
-        <h2>${element.desc}</h2>
-        <button class="read-more">
-            Read More
-        </button>
+        <p>${element.desc}</p>
     </div>
+    <button class="read-more">Read More</button>
 </div>`
 });
